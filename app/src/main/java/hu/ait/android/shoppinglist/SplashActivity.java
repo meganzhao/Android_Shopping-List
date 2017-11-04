00bae548757fedeb44798566a39d3305188780d5
meganzhao <zhaom@carleton.edu>
2017-11-04 09:52:56 +0100

    add SplashActivity

---
+++ app/src/main/java/hu/ait/android/shoppinglist/SplashActivity.java
@@ -0,0 +1,22 @@
+package hu.ait.android.shoppinglist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhaozhaoxia on 11/4/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
